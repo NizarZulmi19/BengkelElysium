4f6bfae92fd3fe36163d01372cbf0dc0a841c3ef
Nizar <nizarsteam19@gmail.com>
2021-01-14 10:04:00 +0700

    Update

---
+++ app/src/main/java/com/nizar/bengkelelysium/ui/main/Profile.kt
@@ -0,0 +1,13 @@
+package com.nizar.bengkelelysium.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nizar.bengkelelysium.R
+
+
+class Profile: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about_me)
+    }
+}
